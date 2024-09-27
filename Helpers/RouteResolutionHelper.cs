476657b889a97746d87078db7707e2bc6866a3b4
Evans Mungai <69681950+EvansMungai@users.noreply.github.com>
2024-09-27 13:34:58 +0300

    Add room routes.

--- Helpers/RouteResolutionHelper.cs
+++ Helpers/RouteResolutionHelper.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using UHB.Models;
+using UHB.Services;
 
 namespace UHB.Helpers
 {
@@ -8,7 +9,6 @@ namespace UHB.Helpers
         public static void addMappings(WebApplication app)
         {
             // Hostel Routes
-
             app.MapGet("/hostels", () => HostelService.GetHostels());
             app.MapGet("/hostels/{id}", (int id) => HostelService.GetHostel(id));
             app.MapPost("/hostels", ([FromBody] Hostel hostel) => HostelService.CreateHostel(hostel));
@@ -19,6 +19,13 @@ namespace UHB.Helpers
             app.MapGet("/student/{id}", (string id) => StudentService.GetStudent(id));
             app.MapPost("/students", ([FromBody] Student student) => StudentService.CreateStudent(student));
             app.MapPut("/students", ([FromBody] Student student) => StudentService.UpdateStudent(student));
+
+            // Room Routes
+            app.MapGet("/rooms", () => RoomService.GetRooms());
+            app.MapGet("/rooms/{id}", (string id) => RoomService.GetRoom(id));
+            app.MapPost("/rooms", ([FromBody] Rooms room)=>RoomService.CreateRoom(room));
+            app.MapPut("/rooms", ([FromBody] Rooms room)=>RoomService.UpdateRoom(room));
+
         }
     }
 }
