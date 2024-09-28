bd20f0521efbef099f13ab34caffb46e72f1d73d
Evans Mungai <69681950+EvansMungai@users.noreply.github.com>
2024-09-28 16:53:45 +0300

    Refactor code in Hostel and Student service.

--- Services/HostelService.cs
+++ Services/HostelService.cs
@@ -1,41 +1,43 @@
-﻿
-using UHB.Models;
+﻿using UHB.Models;
 
-public class HostelService
+namespace UHB.Services
 {
-    public static List<Hostel> _hostels = new List<Hostel>()
+    public class HostelService
+    {
+        public static List<Hostel> _hostels = new List<Hostel>()
     {
         new Hostel{ NO=1, Name="Batian", Capacity=50, Type="Male" },
         new Hostel{ NO=2, Name="Serengeti", Capacity=50, Type="Female" },
         new Hostel{ NO=3, Name="Mt Kenya", Capacity=50, Type="Male" },
         new Hostel{ NO=4, Name="Lenana", Capacity=50, Type="Female" }
     };
-    public static List<Hostel> GetHostels() { return _hostels; }
-    public static Hostel? GetHostel(int id) { return _hostels.SingleOrDefault(hostel => hostel.NO == id); }
-    public static Hostel CreateHostel(Hostel hostel)
-    {
-        _hostels.Add(hostel);
-        return hostel;
-    }
-    public static Hostel? UpdateHostel(Hostel update, int id)
-    {
-        Hostel? hostel = GetHostel(id);
-        if (hostel != null)
+        public static List<Hostel> GetHostels() { return _hostels; }
+        public static Hostel? GetHostel(int id) { return _hostels.SingleOrDefault(hostel => hostel.NO == id); }
+        public static Hostel CreateHostel(Hostel hostel)
         {
-            hostel.NO = update.NO;
-            hostel.Name = update.Name;
-            hostel.Capacity = update.Capacity;
-            hostel.Type = update.Type;
+            _hostels.Add(hostel);
+            return hostel;
         }
-        return hostel;
-    }
-    public static Hostel? RemoveHostel(int id)
-    {
-        Hostel? hostel = GetHostel(id);
-        if(hostel != null)
+        public static Hostel? UpdateHostel(Hostel update, int id)
         {
-            _hostels.Remove(hostel);
+            Hostel? hostel = GetHostel(id);
+            if (hostel != null)
+            {
+                hostel.NO = update.NO;
+                hostel.Name = update.Name;
+                hostel.Capacity = update.Capacity;
+                hostel.Type = update.Type;
+            }
+            return hostel;
         }
-        return hostel;
+        public static Hostel? RemoveHostel(int id)
+        {
+            Hostel? hostel = GetHostel(id);
+            if (hostel != null)
+            {
+                _hostels.Remove(hostel);
+            }
+            return hostel;
+        }
     }
 }
--- Services/StudentService.cs
+++ Services/StudentService.cs
@@ -1,49 +1,53 @@
 ﻿using UHB.Models;
 
-public class StudentService
+namespace UHB.Services
 {
-    public static List<Student> _students = new List<Student>()
+    public class StudentService
+    {
+        public static List<Student> _students = new List<Student>()
     {
         new Student { regNo = "C026-01-0914/2022", surname="Njagi", firstName="Griffins", secondName="Gitari"}
     };
-    public static List<Student> GetStudents() { return _students; }
-    public static Student? GetStudent(string regNo)
-    {
-        regNo = getRegNo(regNo);
-        return _students.SingleOrDefault(Student => Student.regNo == regNo);
-    }
-    public static Student CreateStudent(Student student)
-    {
-        _students.Add(student);
-        return student;
-    }
-    public static Student? UpdateStudent(Student update, string regNo)
-    {
-        Student? student = GetStudent(regNo);
-        if (student != null)
+        public static List<Student> GetStudents() { return _students; }
+        public static Student? GetStudent(string regNo)
         {
-            student.regNo = update.regNo;
-            student.surname = update.surname;
-            student.firstName = update.firstName;
-            student.secondName = update.secondName;
-            student.gender = update.gender;
+            regNo = getRegNo(regNo);
+            return _students.SingleOrDefault(Student => Student.regNo == regNo);
         }
-        return student;
-    }
-    public static Student? RemoveStudent(string regNo)
-    {
-        Student? student = GetStudent(regNo);
-        if (student != null)
+        public static Student CreateStudent(Student student)
         {
-            _students.Remove(student);
+            _students.Add(student);
+            return student;
         }
-        return student;
+        public static Student? UpdateStudent(Student update, string regNo)
+        {
+            Student? student = GetStudent(regNo);
+            if (student != null)
+            {
+                student.regNo = update.regNo;
+                student.surname = update.surname;
+                student.firstName = update.firstName;
+                student.secondName = update.secondName;
+                student.gender = update.gender;
+            }
+            return student;
+        }
+        public static Student? RemoveStudent(string regNo)
+        {
+            Student? student = GetStudent(regNo);
+            if (student != null)
+            {
+                _students.Remove(student);
+            }
+            return student;
+        }
+        #region Utilities
+        private static string getRegNo(string regNo)
+        {
+            regNo = regNo.Replace("%2F", "/");
+            return regNo;
+        }
+        #endregion
     }
-    #region Utilities
-    private static string getRegNo(string regNo)
-    {
-        regNo = regNo.Replace("%2F", "/");
-        return regNo;
-    }
-    #endregion
 }
+
